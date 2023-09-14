ea6f21dbe140d3e459d8b736f94982036a45a35b
xia4243 <xia4243@gmail.com>
2023-09-14 11:32:24 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <element-view></element-view> -->
+    <!-- <emp-view></emp-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import EmpView from './views/tilas/EmpView.vue'
+// import ElementView from './views/element/ElementView.vue'
+export default {
+  // components: { EmpView },
+  // components: { ElementView },
+  data() {
+    return {
+      message: "Hello Vue"
+    }
+  }
+}
+</script>
+
+<style></style>
